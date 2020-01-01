57dcd19f7af1fdbeb3a3780b9cef9398d151fd38
Ociel Lerma <oglerma@gmail.com>
2020-01-01 15:24:07 -0600

    Starting Project

---
+++ GithubFollowers_app/GithubFollowers_app/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GithubFollowers_app
+//
+//  Created by Ociel Lerma on 1/1/20.
+//  Copyright © 2020 Ociel Lerma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
