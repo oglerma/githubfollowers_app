4ea71debbc7188273b810a8932a19abc774248da
Ociel Lerma <oglerma@gmail.com>
2020-01-01 16:52:15 -0600

    Creating a custom button and textfield

---
+++ GithubFollowers_app/GithubFollowers_app/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GithubFollowers_app
+//
+//  Created by Ociel Lerma on 1/1/20.
+//  Copyright © 2020 Ociel Lerma. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    init(backgroundColor: UIColor, title: String){
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+    }
+    
+    func configure(){
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
